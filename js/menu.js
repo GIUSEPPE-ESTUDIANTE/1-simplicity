e354db1aedd438da3c594ff6d470e7ec92b06782
GIUSEPPE-ESTUDIANTE <giusepe002@gmail.com>
2023-10-19 16:32:59 -0300

    alteraçao

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Selecionado o elemento (atraves de descendecia) que acionara o menu
+const botaoMenu = document.querySelector("nav h2");
+
+
+//  Selecionando a lista/menu atraves de classe 
+const menu = document.querySelector(".menu");
+
+
+// Selecionando o link que esta dentro do h2
+const textobotao = document.querySelector("a")
